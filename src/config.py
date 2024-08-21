303e31a9e4850204c2804a805413cf4839077807
Achyutam Mehta <33115122+achyutamm@users.noreply.github.com>
2024-08-21 21:24:03 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@
 # src/config.py
 
 def get_config():
-    openai_api_key = "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" 
+    openai_api_key = "" # add your open api key
     model_name = "gpt-4o-mini"
     return openai_api_key, model_name
